bfa6f5dd683c70bd56f300cf2109adbfdc88f28a
Thiri <thiriyk.25@ichat.sp.edu.sg>
2026-05-15 09:24:23 +0800

    Another testing file with updates

--- price_info.py
+++ price_info.py
@@ -32,14 +32,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-    import price_info as PRICE
-
-def test_find_total_cost():
-    expected_result = 46.75
-    result = PRICE.total_cost_shopping()
-    assert (result == expected_result)
-
-def test_cost_of_fruits():
-    expected_result = 32.50
-    result = PRICE.cost_of_fruits('watermelon', 5)
-    assert (result == expected_result)
